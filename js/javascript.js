7e5aa94e5037a9fa7eb9f2822f81f97c15d8d2dd
david vargas <kaperuzzoo@gmail.com>
2022-04-02 19:29:05 +0200

    comomommomomoomomomom

---
+++ js/javascript.js
@@ -0,0 +1,110 @@
+const caracteristicas=document.getElementById("características-y-gramática");
+caracteristicas.addEventListener("click",(e)=>{
+  let info="snake_case  UPER_CASE  UpperCamelCase  lowerCamelCase primitivos-funciones-instancias"
+    alert(info);
+});
+
+const leta=document.getElementById("var-let");
+leta.addEventListener("click",(e)=>{
+  let info="var es una mala practica let es una muy buena practica"
+    alert(info);
+});
+
+const cas=document.getElementById("constantes");
+cas.addEventListener("click",(e)=>{
+  let info="aprendimos lo que es primitivos-compuestos"
+    alert(info);
+});
+
+const de=document.getElementById("cadenas-de-texto");
+de.addEventListener("click",(e)=>{
+  let info="nos enseño una pagina de mozillavpennetwork para saber mas sobre cadenas de texto"
+    alert(info);
+});
+
+const ve=document.getElementById("template-strings");
+ve.addEventListener("click",(e)=>{
+  let info="concatenacion interpolacion de variables ul-li-ol"
+    alert(info);
+});
+
+const pe=document.getElementById("numbers");
+pe.addEventListener("click",(e)=>{
+  let info="no hay mucho lio"
+    alert(info);
+});
+
+const le=document.getElementById("booleans");
+le.addEventListener("click",(e)=>{
+  let info="true-false  primitivos compuestos"
+    alert(info);
+});
+
+const pelele=document.getElementById("und-null-nan");
+pelele.addEventListener("click",(e)=>{
+  let info="nulo NaN undefind"
+    alert(info);
+});
+
+const lelepe=document.getElementById("funciones");
+lelepe.addEventListener("click",(e)=>{
+  let info="funcion dedicada invocacion de funcion"
+    alert(info);
+});
+
+const hole=document.getElementById("arreglos");
+hole.addEventListener("click",(e)=>{
+  let info="Los arrays son objetos similares a una lista cuyo prototipo proporciona métodos para efectuar operaciones de recorrido y de mutación. Tanto la longitud como el tipo de los elementos de un array son variables"
+    alert(info);
+});
+
+const ht=document.getElementById("objetos");
+ht.addEventListener("click",(e)=>{
+  let info="La clase Object representa uno de los tipos de datos en Javascript. Es usado para guardar una colección de datos definidos y entidades más complejas. Los objetos pueden ser creados utilzando el constructor Object() o la sintaxis literal"
+    alert(info);
+});
+
+const fghio=document.getElementById("operadores");
+fghio.addEventListener("click",(e)=>{
+  let info="Asignación de resta	x -= y	x = x - y Asignación de multiplicación	x *= y	x = x * y Asignación de división	x /= y	x = x / y Asignación de adición	x += y	x = x + y"
+    alert(info);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
